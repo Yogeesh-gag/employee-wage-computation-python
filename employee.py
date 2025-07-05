93e6e75bdd17a5e8b964df3212eeb3a6d0ad158d
Yogi@gag <dr.yogeesh31@gmail.com>
2025-07-05 11:58:55 +0530

    [Yogeesh]feat: add attendance check with daily wage calculation based on presence

--- employee.py
+++ employee.py
@@ -12,4 +12,13 @@ class Employee:
         attendance = random.randint(0, 1)
         print(f"\nAttendance for {name} (ID: {emp_id}): {'Present' if attendance else 'Absent'}")
         return bool(attendance)
-    
+
+    def calculate_daily_wage(self, hours, name, emp_id):
+        daily_wage = 0
+        if not self.check_attendance(name, emp_id):
+            print("Employee is Absent. Daily Wage: 0")
+            return daily_wage
+        else:
+            daily_wage = hours * self.wage_per_hour
+            print(f"Daily Wage: {daily_wage}")
+        return daily_wage
--- main.py
+++ main.py
@@ -4,5 +4,9 @@ if __name__ == "__main__":
     emp = Employee(input("Enter Employee ID: "), input("Enter Employee Name: "), float(input("Enter wage per hour: ")))
     present = Employee.check_attendance(emp.name,emp.emp_id)
 
+    if present:
+        emp.calculate_daily_wage(int(input("Enter how many hours worked: ")),emp.name,emp.emp_id)
+    else:
+        emp.calculate_daily_wage(0,emp.name,emp.emp_id)
 
 
